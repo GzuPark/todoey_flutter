c0492d0cd489c137afbac31d32f703e8be8e0214
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-02-01 19:52:26 +0900

    Apply to show task list using by ListView.builder

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/models/task.dart';
 import 'package:todoey_flutter/widgets/task_tile.dart';
 
-class TasksList extends StatelessWidget {
-  const TasksList({
-    Key? key,
-  }) : super(key: key);
+class TasksList extends StatefulWidget {
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        return TaskTile(
+          taskTile: tasks[index].name,
+          isChecked: tasks[index].isDone!,
+          checkboxCallback: (bool? checkboxState) {
+            setState(() => tasks[index].toggleDone());
+          },
+        );
+      },
     );
   }
 }
