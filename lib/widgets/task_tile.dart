c8d5a787c291d07174ea7cb42927fe087bd7ea2f
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-02-01 23:02:48 +0900

    Add to remove task action with long pressed tile

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/screens/add_task_screen.dart';
 
 class TaskTile extends StatelessWidget {
   final String taskTile;
   final bool isChecked;
   final Function(bool?) checkboxCallback;
+  final Function() longPressCallback;
 
   TaskTile({
     required this.taskTile,
     required this.isChecked,
     required this.checkboxCallback,
+    required this.longPressCallback,
   });
 
   @override
@@ -23,6 +26,7 @@ class TaskTile extends StatelessWidget {
         value: isChecked,
         onChanged: checkboxCallback,
       ),
+      onLongPress: longPressCallback,
     );
   }
 }
