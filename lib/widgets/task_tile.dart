134b7d05920c027e9930aa5e3a3e81cd78c8abf9
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-02-01 18:18:44 +0900

    Apply to text desgin related with checkbox status

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
-  const TaskTile({
-    Key? key,
-  }) : super(key: key);
+class TaskTile extends StatefulWidget {
+  @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Buy milk'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
+      title: Text(
+        'Buy milk',
+        style: TextStyle(decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (bool? checkboxState) {
+          setState(() => isChecked = checkboxState ?? true);
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function(bool?) toggleCheckboxState;
+
+  TaskCheckbox({
+    required this.checkboxState,
+    required this.toggleCheckboxState,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
